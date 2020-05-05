a6d1d043a4cd881494db4e1577c748e4c3e93b31
Stephen Esser <Stephen.Esser@Cerner.com>
2020-05-04 23:03:36 -0500

    Change spec regex patterns to match existing standards

--- packages/terra-functional-testing/src/test-runner/wdio.conf.js
+++ packages/terra-functional-testing/src/test-runner/wdio.conf.js
@@ -17,11 +17,8 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './test/specs/**/*.js',
-  ],
-  // Patterns to exclude.
-  exclude: [
-    // 'path/to/excluded/files'
+    './test*/wdio/**/*-spec.js',
+    './packages/*/test*/wdio/**/*-spec.js',
   ],
   //
   // ============
